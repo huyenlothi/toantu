2633f37d0bce42cc797605d2709d9b12a6b929e7
huyenlothi <69282948+huyenlothi@users.noreply.github.com>
2020-08-10 23:17:25 +0700

    sudungtoantu

---
+++ bai1.js
@@ -0,0 +1,8 @@
+var inputWidth;
+var inputHeight;
+inputWidth = prompt("enter the width");
+inputHeight = prompt("enter the height");
+var width = parseInt(inputWidth);
+var height = parseInt(inputHeight);
+var area = width * height;
+document.write("the area is: " + area);
